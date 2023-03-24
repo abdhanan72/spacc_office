82e73c942250fde6a3101d92624827889503e757
abdhanan72 <abdhananvattoli@gmail.com>
2023-03-24 14:50:03 +0530

    zxxx

--- lib/Receipt/receipt.dart
+++ lib/Receipt/receipt.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class ReceiptEntry extends StatelessWidget {
+class ReceiptEntry extends StatefulWidget {
   const ReceiptEntry({super.key});
 
+
+  
+
+  @override
+  State<ReceiptEntry> createState() => _ReceiptEntryState();
+
+  
+}
+
+class _ReceiptEntryState extends State<ReceiptEntry> {
+
+String? username;
+
+
+  @override
+  void initState() async{
+   SharedPreferences prefs = await SharedPreferences.getInstance();
+ username = prefs.getString('username');
+
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         title: const Text('Receipt Entry'),
+        
       ),
+
+      body:  Center(child: Text(username!)),
     );
   }
 }
